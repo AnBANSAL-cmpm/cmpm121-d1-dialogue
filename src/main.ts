a1718293b24df34a52012e9608a8a774821c261d
AnBANSAL-cmpm <abansal6@ucsc.edu>
2025-10-08 02:48:29 -0700

    Added button for dialogue

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
 
-// Now add the button (won't be wiped out because it's appended after)
-const button = document.createElement('button');
-button.textContent = 'Talk';
+const button = document.createElement("button");
+button.textContent = "Talk";
 document.body.appendChild(button);
